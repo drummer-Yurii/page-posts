fdd4666d2c601e41f792f993eb74ecb0f9ca0b7b
Yurii Rybachok <drummer2009@ukr.net>
2022-12-20 17:00:14 +0200

    add posts on page

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,89 @@
 <template>
-    <div>
-        <div class="post">
-            <div><strong>Название:</strong> Пост о JavaScript</div>
-            <div><strong>Описание:</strong> JavaScript универсальный язык программирования</div>
+    <div class="app">
+        <form>
+            <h4>Создание поста</h4>
+            <input class="input" type="text" placeholder="Название">
+            <input class="input" type="text" placeholder="Описание">
+            <button class="btn" @click="createPost">Создать</button>
+        </form>
+        <div class="post" v-for="post in posts">
+            <div><strong>Название:</strong>{{ post.title }}</div>
+            <div><strong>Описание:</strong>{{ post.body }}</div>
         </div>
+
     </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                likes: 0,
-                dislikes: 0,
-            }
-        },
-        methods: {
-            addLike() {
-                this.likes += 1;
-            },
-            addDislike() {
-                this.dislikes += 1;
-            }
+export default {
+    data() {
+        return {
+            posts: [
+                {
+                    id: 1,
+                    title: 'JavaScript',
+                    body: 'Описание поста'
+                },
+                {
+                    id: 2,
+                    title: 'JavaScript 2',
+                    body: 'Описание поста 2'
+                },
+                {
+                    id: 3,
+                    title: 'JavaScript 3',
+                    body: 'Описание поста 3'
+                },
+                {
+                    id: 4,
+                    title: 'JavaScript 4',
+                    body: 'Описание поста 4'
+                },
+            ]
+        }
+    },
+    methods: {
+        createPost() {
+            
         }
     }
+}
 </script>
 
 <style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
+.app {
+    padding: 20px;
+}
+
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+}
+form {
+    display: flex;
+    flex-direction: column;
+
+}
+.input {
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+}
+.btn {
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+
+}
 </style>
