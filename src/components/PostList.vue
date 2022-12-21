a332db3cb38264de8fff2a45b425c254143daecf
Yurii Rybachok <drummer2009@ukr.net>
2022-12-21 13:20:48 +0200

    show msg after delete posts

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,11 +1,16 @@
 <template>
-    <div>
-        <h3>Список пользователей</h3>
+    <div v-if="posts.length > 0">
+        <h3>Список постов</h3>
         <post-item 
             v-for="post in posts"
-            :post="post" 
+            :post="post"
+            :key="post.id"
+            @remove="$emit('remove', post)" 
         />
     </div>
+    <h2 v-else style="color: red">
+        Список постов пуст
+    </h2>
 </template>
 <script>
 import PostItem from '@/components/PostItem.vue'
