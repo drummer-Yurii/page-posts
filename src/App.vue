6bc97ba232b2c72f20e6b43c10538e30a189ea5d
Yurii Rybachok <drummer2009@ukr.net>
2022-12-22 08:02:57 +0200

    sorted arr posts whith computed

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
             <post-form @create="createPost" />
         </my-dialog>
 
-        <post-list :posts="posts" @remove="removePost" v-if="!isPostsLoading" />
+        <post-list :posts="sortedPosts" @remove="removePost" v-if="!isPostsLoading" />
         <div v-else>Идет загрузка...</div>
     </div>
 </template>
@@ -70,13 +70,12 @@ export default {
     mounted() {
         this.fetchPosts();
     },
-    watch: {
-        selectedSort(newValue) {
-            this.posts.sort((post1, post2) => {
-                return post1[newValue]?.localeCompare(post2[newValue])
-            })
-        },
-    }
+    computed: {
+        sortedPosts() {
+            return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+        }
+    },
+    watch: {},
 }
 </script>
 
