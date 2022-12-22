f2b4d11c3620034a5d23a73bf86f6c4461cb46e1
Yurii Rybachok <drummer2009@ukr.net>
2022-12-22 11:08:14 +0200

    add and use custom durectives

--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -4,6 +4,7 @@
         <my-input 
             v-model="searchQuery"
             placeholder="Поиск..."
+            v-focus
         />
         <div class="app__btns">
             <my-button @click="showDialog">
@@ -24,7 +25,7 @@
             v-if="!isPostsLoading" 
         />
         <div v-else>Идет загрузка...</div>
-        <div ref="observer" class="observer"></div>
+        <div v-intersection="loadMorePosts" class="observer"></div>
         <!-- <div class="page__wrapper">
             <div 
                 v-for="pageNumber in totalPages" 
@@ -120,17 +121,17 @@ export default {
     mounted() {
         this.fetchPosts();
         console.log(this.$refs.observer);
-        const options = {
-            rootMargin: '0px',
-            threshold: 1.0
-        }
-        const callback = (entries, observer) => {
-            if (entries[0].isIntersecting && this.page < this.totalPages) {
-                this.loadMorePosts()
-            }
-        };
-        const observer = new IntersectionObserver(callback, options); 
-        observer.observe(this.$refs.observer);
+        // const options = {
+        //     rootMargin: '0px',
+        //     threshold: 1.0
+        // }
+        // const callback = (entries, observer) => {
+        //     if (entries[0].isIntersecting && this.page < this.totalPages) {
+        //         this.loadMorePosts()
+        //     }
+        // };
+        // const observer = new IntersectionObserver(callback, options); 
+        // observer.observe(this.$refs.observer);
     },
     computed: {
         sortedPosts() {
